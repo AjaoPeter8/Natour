c4561785adf6869c4e99d0adea3c0883eea02529
ajaopeter8 <oluwafemipeter02@gmail.com>
2026-01-26 22:33:32 +0100

    implemented error handling for wrong urls on our server

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import tourRouter from './routes/tourRoute.js';
 import morgan from 'morgan';
 import qs from 'qs';
+import AppError from './utils/appError.js';
+import globalErrorHandler from './controllers/errorController.js';
 
 const app = express();
 app.use(express.json());
@@ -9,12 +11,15 @@ app.set('query parser', str => qs.parse(str));
 
 app.use(express.static(`${import.meta.dirname}/public`));
 app.use(morgan('dev'));
-// app.set('query parser', 'extended'); // Add this to ensure req.query is a standard object
-// app.set('query parser fn', (str) => qs.parse(str));
-
 
 
 // ROUTES
 app.use('/api/v1/tours', tourRouter);
+
+app.use((req, res, next) => {
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`,  404));
+});
+
+app.use(globalErrorHandler);
 
 export default app;
