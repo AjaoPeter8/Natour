43aa69cd46d8bd8fd7d854d1d2e6568a8660908d
ajaopeter8 <oluwafemipeter02@gmail.com>
2026-02-12 12:51:39 +0100

    implemented data sanitization and handled parameter pollution

--- app.js
+++ app.js
@@ -6,32 +6,47 @@ import qs from 'qs';
 import AppError from './utils/appError.js';
 import globalErrorHandler from './controllers/errorController.js';
 import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';    
+import mongoSanitize from '@exortek/express-mongo-sanitize'; 
+import hpp from 'hpp';
 
 const app = express();
-app.use(express.json());
-app.set('query parser', str => qs.parse(str));
 
-app.use(express.static(`${import.meta.dirname}/public`));
+// Set Security HHTP headers
+app.use(helmet());
 
-if (process.env.NODE_ENV === 'development'){
-app.use(morgan('dev'));
+//Development logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 
-const limiter = rateLimit ({
-    max: 100,
-    windowMs: 60*60*1000,
-    message: 'Too many requests. Try again in an hour'
+// Limits requests from same IP address
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests. Try again in an hour',
 });
-
 app.use('/api', limiter);
 
+//Body Parser, reading data from the body into req.body
+app.use(express.json({ limit: '10kb' }));
+app.set('query parser', (str) => qs.parse(str));
 
+//Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
 
+//Preventing Parameter Pollution
+app.use(hpp({
+    whitelist: ['duration', 'ratingsQuantity', 'ratingsAverage', 'maxGroupSize', 'difficulty', 'price']
+}))
 
+//Serve static files
+app.use(express.static(`${import.meta.dirname}/public`));
+
+// Test middleware
 app.use((req, res, next) => {
-    req.requestTime = new Date().toISOString();
-    next();
-    
+  req.requestTime = new Date().toISOString();
+  next();
 });
 
 // ROUTES
@@ -39,7 +54,7 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.use((req, res, next) => {
-    next(new AppError(`Can't find ${req.originalUrl} on this server!`,  404));
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
 app.use(globalErrorHandler);
